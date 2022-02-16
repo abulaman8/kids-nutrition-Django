a2076c11e6091554afb7334bb162b642d1a8e455
5.413U14M4N <abulaman6@gmail.com>
2022-02-16 12:45:41 +0530

    created models

--- kids/admin.py
+++ kids/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Kids, Image
 
 # Register your models here.
+
+admin.site.register(Kids)
+admin.site.register(Image)
--- kids/models.py
+++ kids/models.py
@@ -1,3 +1,30 @@
+
 from django.db import models
 
 # Create your models here.
+class Kids(models.Model):
+    kid_name = models.CharField(max_length=100, null=False, blank=False)
+    kid_age = models.IntegerField(null=False, blank=False)
+    parent_phone_number = models.CharField(max_length=12, null=False, blank=False)
+    parent_email_address = models.EmailField(null=False, blank=False)
+
+    def __str__(self):
+        return self.kid_name
+
+
+class Image(models.Model):
+    CHOICES = (
+        ('FRUIT', 'Fruit'),
+        ('VEGETABLE', 'Vegetable'),
+        ('GRAIN', 'Grain'),
+        ('DAIRY', 'Dairy'),
+        ('PROTIEN', 'Protien'),
+        ('UNKNOWN', 'Unknown')
+    )
+    kid = models.ForeignKey(Kids, on_delete=models.CASCADE, null=False, blank=False)
+    image_url = models.URLField(null=False, blank=False)
+    created_on = models.DateTimeField(null=False, blank=False)
+    updated_on = models.DateTimeField(null=False, blank=False)
+    is_approved = models.BooleanField(default=False, null=False, blank=False)
+    approoved_by = models.CharField(max_length=100, null=False, blank=False)
+    food_group = models.CharField(choices=CHOICES, default='UNKNOWN', max_length=15)
