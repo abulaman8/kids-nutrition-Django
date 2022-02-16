12d18e5c8fbbb5cbb2ad06b47cd18753f7a7c2d5
5.413U14M4N <abulaman6@gmail.com>
2022-02-16 14:31:01 +0530

    email implemented

--- alemeno/settings.py
+++ alemeno/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'tinystarkfordjango@gmail.com'
+EMAIL_HOST_PASSWORD = 'djangoemail8'
+EMAIL_USE_TLS = True
--- kids/models.py
+++ kids/models.py
@@ -1,5 +1,6 @@
 
 from django.db import models
+from django.core.mail import send_mail
 
 # Create your models here.
 class Kids(models.Model):
@@ -28,3 +29,16 @@ class Image(models.Model):
     is_approved = models.BooleanField(default=True, null=False, blank=False)
     approoved_by = models.CharField(max_length=100, null=False, blank=False)
     food_group = models.CharField(choices=CHOICES, default='UNKNOWN', max_length=15)
+
+    def email(self):
+        send_mail(
+            'Notifying missing food in image',
+            f'Dear Parent, \n    The image uploaded for your kid {self.kid.kid_name}\'s  schedule doesn\'t have a food in it, Please make sure the kid eats what you give him. \n With Love, \n Team Alemeno.',
+            'tinystarkfordjango@gmail.com',
+            [f'{self.kid.parent_email_address}'],
+        )
+    def save(self, *args, **kwargs):
+        if not self.is_approved:
+            self.email()
+            super(Image, self).save(*args, **kwargs)
+        super(Image, self).save(*args, **kwargs)
